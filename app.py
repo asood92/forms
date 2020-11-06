a4261c64791c163beb3e2d1ff12a13567a170d1c
Amman Sood <392075+asood92@users.noreply.github.com>
2020-11-05 19:20:59 -0500

    part 1 done

--- app.py
+++ app.py
@@ -3,49 +3,86 @@ import random
 
 app = Flask(__name__)
 
+
 def sort_letters(message):
     """A helper method to sort the characters of a string in alphabetical order
     and return the new string."""
-    return ''.join(sorted(list(message)))
+    return "".join(sorted(list(message)))
 
 
-@app.route('/')
+@app.route("/")
 def homepage():
     """A homepage with handy links for your convenience."""
-    return render_template('home.html')
+    return render_template("home.html")
 
-@app.route('/froyo')
+
+@app.route("/froyo")
 def choose_froyo():
     """Shows a form to collect the user's Fro-Yo order."""
-    pass
+    return """
+    <form action="/froyo_results" method="GET">
+        What is your favorite Fro-Yo flavor? <br/>
+        <input type="text" name="flavor"><br/>
+        <input type="text" name="toppings"><br/>
+        <input type="submit" value="Submit!">
+    </form>
+    """
 
-@app.route('/froyo_results')
+
+@app.route("/froyo_results")
 def show_froyo_results():
-    """Shows the user what they ordered from the previous page."""
-    pass
+    users_froyo_flavor = request.args.get("flavor")
+    user_toppings = request.args.get("toppings")
+    return f"You ordered {users_froyo_flavor} flavored Fro-Yo with {user_toppings}!"
 
-@app.route('/favorites')
+
+@app.route("/favorites")
 def favorites():
     """Shows the user a form to choose their favorite color, animal, and city."""
-    pass
+    return """
+    <form action="/favorite_results" method="GET">
+        What is your favorite color? <br/>
+        <input type="text" name="color"><br/>
+        What is your favorite animal? <br/>
+        <input type="text" name="animal"><br/>
+        What is your favorite city? <br/>
+        <input type="text" name="city">
+        <input type="submit" value="Submit">
+    </form>
+    """
 
-@app.route('/favorites_results')
+
+@app.route("/favorites_results")
 def favorites_results():
     """Shows the user a nice message using their form results."""
-    pass
+    user_color = request.args.get("color")
+    user_animal = request.args.get("animal")
+    user_city = request.args.get("city")
+    return f"Wow I didn't know {user_color} {user_animal} lived in {user_city}"
 
-@app.route('/secret_message')
+
+@app.route("/secret_message")
 def secret_message():
     """Shows the user a form to collect a secret message. Sends the result via
     the POST method to keep it a secret!"""
-    pass
+    return """
+    <form action="/message_results" method="POST">
+        Enter a secret message <br/>
+        <input type="text" name="message">
+        <input type="submit" name="Submit">
+    </form>
+    """
 
-@app.route('/message_results', methods=['POST'])
+
+@app.route("/message_results", methods=["POST"])
 def message_results():
     """Shows the user their message, with the letters in sorted order."""
-    pass
+    user_message = request.args.get("message")
+    user_message = user_message.sort_letters()
+    return f"Here's your secret message! \n {user_message}"
 
-@app.route('/calculator')
+
+@app.route("/calculator")
 def calculator():
     """Shows the user a form to enter 2 numbers and an operation."""
     return """
@@ -63,55 +100,69 @@ def calculator():
     </form>
     """
 
-@app.route('/calculator_results')
+
+@app.route("/calculator_results")
 def calculator_results():
     """Shows the user the result of their calculation."""
-    pass
+    numberOne = request.args.get("operand1")
+    numberTwo = request.args.get("operand2")
+    mathOperation = request.args.get("operation")
+    if mathOperation == "add":
+        return f"You chose to {mathOperation} {numberOne} {numberTwo}. Your result is {int(numberOne) + int(numberTwo)}"
+    elif mathOperation == "subtract":
+        return f"You chose to {mathOperation} {numberOne} {numberTwo}. Your result is {int(numberOne) - int(numberTwo)}"
+
+    elif mathOperation == "multiply":
+        return f"You chose to {mathOperation} {numberOne} {numberTwo}. Your result is {int(numberOne) * int(numberTwo)}"
+    else:
+        return f"You chose to {mathOperation} {numberOne} {numberTwo}. Your result is {int(numberOne) / int(numberTwo)}"
 
 
-# List of compliments to be used in the `compliments_results` route (feel free 
-# to add your own!) 
+# List of compliments to be used in the `compliments_results` route (feel free
+# to add your own!)
 # https://systemagicmotives.com/positive-adjectives.htm
 list_of_compliments = [
-    'awesome',
-    'beatific',
-    'blithesome',
-    'conscientious',
-    'coruscant',
-    'erudite',
-    'exquisite',
-    'fabulous',
-    'fantastic',
-    'gorgeous',
-    'indubitable',
-    'ineffable',
-    'magnificent',
-    'outstanding',
-    'propitioius',
-    'remarkable',
-    'spectacular',
-    'splendiferous',
-    'stupendous',
-    'super',
-    'upbeat',
-    'wondrous',
-    'zoetic'
+    "awesome",
+    "beatific",
+    "blithesome",
+    "conscientious",
+    "coruscant",
+    "erudite",
+    "exquisite",
+    "fabulous",
+    "fantastic",
+    "gorgeous",
+    "indubitable",
+    "ineffable",
+    "magnificent",
+    "outstanding",
+    "propitioius",
+    "remarkable",
+    "spectacular",
+    "splendiferous",
+    "stupendous",
+    "super",
+    "upbeat",
+    "wondrous",
+    "zoetic",
 ]
 
-@app.route('/compliments')
+
+@app.route("/compliments")
 def compliments():
     """Shows the user a form to get compliments."""
-    return render_template('compliments_form.html')
+    return render_template("compliments_form.html")
 
-@app.route('/compliments_results')
+
+@app.route("/compliments_results")
 def compliments_results():
     """Show the user some compliments."""
     context = {
         # TODO: Enter your context variables here.
     }
 
-    return render_template('compliments_results.html', **context)
+    return render_template("compliments_results.html", **context)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
